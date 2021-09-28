1b310ccac230a5125f62503c8aae2b35bd045975
mohamed00Sayed <sayedm22529@gmail.com>
2021-09-28 16:25:40 +0200

    refactor: expected functionality

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,28 +1,58 @@
+import { createTemplateResults, createTableBody } from './createTemplate'
+import { create } from './Creator'
+const template = createTemplateResults()
 function handleSubmit(event) {
-    event.preventDefault()
-
-    // check what text was put into the form field
-    let url = document.getElementById('name').value
-    Client.checkForName(url)
-    const data = {
-      url: url
+  event.preventDefault()
+  const resultSection = document.querySelector('#result')
+  
+  // check validity of text as url
+  let url = document.getElementById('name').value
+  if(Client.checkURL(url)){
+  
+    if(resultSection.hasChildNodes()){
+      const table = document.querySelector('#result-table')
+      const oldTableBody = document.querySelector('#tb-body')
+      table.removeChild(oldTableBody)
+      resultSection.removeChild(template)
     }
-   
+    const loading = create('div',null, 'Result is loading...')
+    resultSection.appendChild(loading)
+    
+
+    const data = {
+      url
+    }
+    
     console.log("::: Form Submitted ::: url is:", url)
+    
     fetch('/analyze',{
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      credentials: 'same-origin', // include, *same-origin, omit
+      method: 'POST', 
+      credentials: 'same-origin', 
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
+      body: JSON.stringify(data) 
     })
-    .then(res => res.json())
+    .then(res => {
+      console.log(res)
+      return res.json()
+    })
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+      console.log("::: Passed :::")
+      const data = res
+      resultSection.removeChild(loading)
+      const tableBody = createTableBody([data])
+      if (!resultSection.firstChild) {
+        resultSection.appendChild(template)
+        const table = document.querySelector('#result-table')
+        table.appendChild(tableBody)
+      }
     })
     .catch(err => console.log(err))
+  }
+  else{
+    alert('Enter a valid link')
+  }
 }
-
 
 export { handleSubmit }
